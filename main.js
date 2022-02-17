664aa2c9df27fd83b5a45fe122958ba1a5681987
aidanshea1 <57193863+aidanshea1@users.noreply.github.com>
2022-02-16 23:19:03 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+fetch('assets/data.json')
+.then(response => {
+  return response.json();
+})
+.then(data => {
+  console.log(data);
+    // render HTML here
+    document.querySelector("main").innerHTML=`
+	  <h1>Data successfully loaded!</h1>
+	  <p>
+	    Name: <strong>${data.about.name}</strong><br>
+	    Number of Projects: <strong>${data.projects.length}</strong>
+	  </p>  
+  `;
+});
